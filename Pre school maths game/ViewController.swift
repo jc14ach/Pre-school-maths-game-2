d4e7bd2332424c0769b7751513699dcbbe26ea9e
jc14ach <jc14ach@cta-ab271-17.herts.ac.uk>
2017-02-20 17:58:29 +0000

    Initial Commit

---
+++ Pre school maths game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pre school maths game
+//
+//  Created by jc14ach on 20/02/2017.
+//  Copyright © 2017 jc14ach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
